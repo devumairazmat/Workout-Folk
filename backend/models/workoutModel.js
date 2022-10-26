8a787603f1fd092d7cdda16ac4a886cd9f759139
devumairazmat <umairazmat8260@gmail.com>
2022-10-27 00:23:48 +0500

    workout schema designed

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,25 @@
+const { default: mongoose } = require("mongoose");
+
+const workoutSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
+
+Workout.find();

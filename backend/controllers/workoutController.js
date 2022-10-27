23fe0ec33f5dc624cfd10e73f6ade09abceaf599
devumairazmat <umairazmat8260@gmail.com>
2022-10-28 00:08:31 +0500

    post workout get all wotkouts / by id done

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,12 +1,31 @@
+const { json } = require("express");
 const Workout = require("../models/workoutModel");
+const mongoose = require("mongoose");
 
 // Get all Workouts
-const getWorkouts = async (req, res) => {};
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  res.status(200).json(workouts);
+};
 
 // Get a Single Workout
-const getWorkout = async (req, res) => {};
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
 // POST a new workout
+
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
   // add doc to DB

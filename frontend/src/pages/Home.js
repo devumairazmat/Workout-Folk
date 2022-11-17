8b04c785852aeadc2326fed0f036e33cd9ede8e0
devumairazmat <umairazmat8260@gmail.com>
2022-11-17 23:58:21 +0500

    Workouts fetched on React App

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
 function Home() {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails workout={workout} key={workout._id} />
+          ))}
+      </div>
     </div>
   );
 }
